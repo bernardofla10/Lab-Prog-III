5a1cd950ae4b68b4b6225c471a6499f18553457d
Bernardo Castro <bernardofla10ps4@gmail.com>
2026-05-14 15:21:25 -0300

    Refactor comments in fetchPostsSequentially function for clarity and detail

--- cap-3/asynchronous-javascript/ex8.js
+++ cap-3/asynchronous-javascript/ex8.js
@@ -2,11 +2,12 @@
 // faz requisições a API para buscar o conteúdo dos posts.
 // A função deve aguardar o resultado de um post antes de buscar o próximo.
 
-const fetchPostsSequentially = async(postIds) => {
+const fetchPostsSequentially = async(postIds) => { // keyword async declara que a função é assíncrona -> permite o uso de await e retorna uma Promise.
     try{
         for(const id of postIds){ // loop tradicional para garantir a sequecialidade. Diferente do forEach, loop for em uma função async respeita as pausas do await.
             // Inicia a requisição e aguarda a resposta do servidor
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/`);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/`); // await pausa a execução da função e aguarda até que a Promise do fetch() seja resolvida.
+            // impede que a próxima requisição seja enviada antes da atual terminar.
 
             // Verifica se a resposta foi bem sucedida (status 200-299)
             if(!response.ok){
